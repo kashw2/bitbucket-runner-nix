0d292ff22ff4d33c768329292fb938fcf14eeb94
kashw2 <supra4keanu@hotmail.com>
2024-12-15 14:17:18 +1000

    refactor: nixfmt

--- package.nix
+++ package.nix
@@ -1,4 +1,8 @@
-{ writeShellScript, fetchzip, buildFHSEnv}:
+{
+  writeShellScript,
+  fetchzip,
+  buildFHSEnv,
+}:
 let
   version = "3.10.0";
   src = fetchzip {
@@ -7,16 +11,17 @@ let
     hash = "sha256-nFRh6hy49Vg8dMfD0ZPyu9ZGuWaLtmxcPXaDl5ePFeM=";
   };
 in
-  buildFHSEnv {
-    name = "bitbucket-runner-linux-shell";
-    targetPkgs = pkgs: (with pkgs; [
+buildFHSEnv {
+  name = "bitbucket-runner-linux-shell";
+  targetPkgs =
+    pkgs:
+    (with pkgs; [
       jre
       bash
       git
     ]);
-    runScript = writeShellScript "bitbucket-runner-linux-shell-start" 
-    ''
+  runScript = writeShellScript "bitbucket-runner-linux-shell-start" ''
     cd "${src}/bin"
     exec bash ./start.sh "$@"
-    '';
-  }
+  '';
+}
