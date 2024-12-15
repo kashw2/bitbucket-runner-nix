4f9ded5b0b6eccbe6562bd4458214ae227e53491
kashw2 <supra4keanu@hotmail.com>
2024-12-15 14:17:00 +1000

    feat: added flake.nix

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Nix Bitbucket Runner Linux Shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+    }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      packages.${system} = with nixpkgs.legacyPackages.${system}.pkgs; {
+        bitbucket-runner = import ./package.nix {
+          inherit
+            writeShellScript
+            fetchzip
+            buildFHSEnv
+            pkgs;
+        };
+      };
+      nixosModules = with nixpkgs.legacyPackages.${system}.pkgs; {
+        bitbucket-runner = import ./default.nix {
+          inherit lib pkgs config;
+        };
+      };
+    };
+}
