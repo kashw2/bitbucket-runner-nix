963273ab0b518bdbd69e4f4c4fdb71e21801ac72
kashw2 <supra4keanu@hotmail.com>
2024-12-16 23:26:43 +1000

    refactor(module): nixfmt

--- default.nix
+++ default.nix
@@ -1,75 +1,71 @@
 # Bitbucket Runner Linux Shell Nix Module
 
 {
-  config,
-  pkgs,
   lib,
+  pkgs,
+  config,
   ...
 }:
 
 with lib;
 
 let
-  cfg = config.services.bitbucket-runner-linux-shell;
+  cfg = config.services.bitbucket-runner;
   bitbucketRunner = pkgs.callPackage ./package.nix { };
 in
 {
-  options = {
-    services.bitbucket-runner-linux-shell = {
-      enable = mkOption {
-        type = types.bool;
-        default = false;
-        description = "Enable the Bitbucket runner service";
-      };
+  imports = [ ];
 
-      user = mkOption {
+  options.services.bitbucket-runner = {
+    enable = mkEnableOption "bitbucket-runner";
+
+    user = mkOption {
+      type = types.str;
+      default = "bitbucket-runner";
+      description = "The user that runs the Bitbucket runner service";
+    };
+
+    group = mkOption {
+      type = types.str;
+      default = "bitbucket-runner";
+      description = "The group for the Bitbucket runner service";
+    };
+
+    flags = {
+      accountUuid = mkOption {
         type = types.str;
-        default = "bitbucket-runner-linux-shell";
-        description = "The user that runs the Bitbucket runner service";
+        description = "The account UUID for the Bitbucket runner";
       };
-
-      group = mkOption {
+      repositoryUuid = mkOption {
         type = types.str;
-        default = "bitbucket-runner-linux-shell";
-        description = "The group for the Bitbucket runner service";
+        description = "The repository UUID for the Bitbucket runner";
       };
-
-      flags = {
-        accountUuid = mkOption {
-          type = types.str;
-          description = "The account UUID for the Bitbucket runner";
-        };
-        repositoryUuid = mkOption {
-          type = types.str;
-          description = "The repository UUID for the Bitbucket runner";
-        };
-        runnerUuid = mkOption {
-          type = types.str;
-          description = "The runner UUID for the Bitbucket runner";
-        };
-        OAuthClientId = mkOption {
-          type = types.str;
-          description = "The OAuth Client ID for Bitbucket authentication";
-        };
-        OAuthClientSecret = mkOption {
-          type = types.str;
-          description = "The OAuth Client Secret for Bitbucket authentication";
-        };
-        workingDirectory = mkOption {
-          type = types.str;
-          default = "/tmp";
-          description = "The working directory for the Bitbucket runner";
-        };
-        runtime = mkOption {
-          type = types.str;
-          default = "linux-shell";
-          description = "The runtime environment for the Bitbucket runner";
-        };
-        extraFlags = mkOption {
-          type = types.str;
-          default = "";
-          description = "Additional flags to pass to the Bitbucket runner";
-        };
+      runnerUuid = mkOption {
+        type = types.str;
+        description = "The runner UUID for the Bitbucket runner";
+      };
+      OAuthClientId = mkOption {
+        type = types.str;
+        description = "The OAuth Client ID for Bitbucket authentication";
+      };
+      OAuthClientSecret = mkOption {
+        type = types.str;
+        description = "The OAuth Client Secret for Bitbucket authentication";
+      };
+      workingDirectory = mkOption {
+        type = types.str;
+        default = "/tmp";
+        description = "The working directory for the Bitbucket runner";
+      };
+      runtime = mkOption {
+        type = types.str;
+        default = "linux-shell";
+        description = "The runtime environment for the Bitbucket runner";
+      };
+      extraFlags = mkOption {
+        type = types.str;
+        default = "";
+        description = "Additional flags to pass to the Bitbucket runner";
       };
     };
   };
@@ -83,8 +79,8 @@ in
 
     users.groups.${cfg.group} = { };
 
-    systemd.services.bitbucket-runner-linux-shell = {
-      description = "Bitbucket Runner Linux Shell Service";
+    systemd.services.bitbucket-runner = {
+      description = "Bitbucket Runner Service";
       after = [ "network.target" ];
       wantedBy = [ "multi-user.target" ];
       serviceConfig = {
