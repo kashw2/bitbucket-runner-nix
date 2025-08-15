a857ab8b7e03bea8185e02199ad032d0a07b6947
kashw2 <supra4keanu@hotmail.com>
2025-08-15 20:00:14 +1000

    feat(module): add support for multiple runner instances

--- default.nix
+++ default.nix
@@ -17,123 +17,132 @@ in
   imports = [ ];
 
   options.services.bitbucket-runner = {
-    enable = mkEnableOption "bitbucket-runner";
+    enable = mkEnableOption "Enable Bitbucket runner module";
+
     user = mkOption {
       type = types.str;
       default = "bitbucket-runner";
-      description = "The user that runs the Bitbucket runner service";
+      description = "User to run Bitbucket runners";
     };
+
     group = mkOption {
       type = types.str;
       default = "bitbucket-runner";
-      description = "The group for the Bitbucket runner service";
+      description = "Group for Bitbucket runners";
     };
-    flags = {
-      accountUuid = mkOption {
-        type = types.str;
-        description = "The account UUID for the Bitbucket runner";
-      };
-      repositoryUuid = mkOption {
-        type = types.nullOr types.str;
-        description = "The repository UUID for the Bitbucket runner";
-        default = null;
-      };
-      runnerUuid = mkOption {
-        type = types.str;
-        description = "The runner UUID for the Bitbucket runner";
-      };
-      OAuthClientId = mkOption {
-        type = types.str;
-        description = "The OAuth Client ID for Bitbucket authentication";
-      };
-      OAuthClientSecret = mkOption {
-        type = types.str;
-        description = "The OAuth Client Secret for Bitbucket authentication";
-      };
-      workingDirectory = mkOption {
-        type = types.str;
-        default = "/tmp";
-        description = "The working directory for the Bitbucket runner";
-      };
-      runtime = mkOption {
-        type = types.enum [
-          "linux-shell"
-          "linux-docker"
-        ];
-        default = "linux-shell";
-        description = "The runtime environment for the Bitbucket runner";
-      };
-      extraFlags = mkOption {
-        type = types.str;
-        default = "";
-        description = "Additional flags to pass to the Bitbucket runner";
-      };
-    };
+
     extraPackages = mkOption {
       type = types.listOf types.package;
-      description = "Additional packages to make available to pipelines";
       default = [ ];
+      description = "Packages available to runner environments";
+    };
+
+    runners = mkOption {
+      type = types.attrsOf (
+        types.submodule {
+          options = {
+            accountUuid = mkOption {
+              type = types.str;
+              description = "Account UUID";
+            };
+            repositoryUuid = mkOption {
+              type = types.nullOr types.str;
+              default = null;
+              description = "Repository UUID";
+            };
+            runnerUuid = mkOption {
+              type = types.str;
+              description = "Runner UUID";
+            };
+            OAuthClientId = mkOption {
+              type = types.str;
+              description = "OAuth Client ID";
+            };
+            OAuthClientSecret = mkOption {
+              type = types.str;
+              description = "OAuth Client Secret";
+            };
+            workingDirectory = mkOption {
+              type = types.str;
+              default = "/tmp";
+              description = "Working directory";
+            };
+            runtime = mkOption {
+              type = types.enum [
+                "linux-shell"
+                "linux-docker"
+              ];
+              default = "linux-shell";
+              description = "Runtime environment";
+            };
+            extraFlags = mkOption {
+              type = types.str;
+              default = "";
+              description = "Extra flags";
+            };
+          };
+        }
+      );
+      default = { };
+      description = "Configuration for Bitbucket Runners.";
     };
   };
 
   config = mkIf cfg.enable {
     users.users.${cfg.user} = {
-      description = "Bitbucket Runner Linux Shell User";
-      group = cfg.group;
-      # For certain File System actions, the runner may need an actual user
-      # An example is Nix operations (eg Colmena, deploy-rs etc)
       isNormalUser = true;
       createHome = true;
       home = "/home/${cfg.user}";
+      group = cfg.group;
+      description = "Bitbucket Runner User";
     };
 
     users.groups.${cfg.group} = { };
 
-    systemd.services.bitbucket-runner = mkIf (cfg.flags.runtime == "linux-shell") {
-      description = "Bitbucket Runner Service";
-      after = [ "network.target" ];
-      wantedBy = [ "multi-user.target" ];
-      path = cfg.extraPackages;
-      serviceConfig = {
-        ExecStart = ''
-          ${bitbucketRunner}/bin/bitbucket-runner-linux-shell \
-            --accountUuid {${cfg.flags.accountUuid}} \
-            --runnerUuid {${cfg.flags.runnerUuid}} \
-            --OAuthClientId ${cfg.flags.OAuthClientId} \
-            --OAuthClientSecret ${cfg.flags.OAuthClientSecret} \
-            --runtime ${cfg.flags.runtime} \
-            --workingDirectory ${cfg.flags.workingDirectory} \
-            ${lib.optionalString (
-              cfg.flags.repositoryUuid != null
-            ) "--repositoryUuid {${cfg.flags.repositoryUuid}}"}
-        '';
-        User = cfg.user;
-        Group = cfg.group;
-        Restart = "on-failure";
-      };
-    };
-
-    virtualisation.oci-containers = mkIf (cfg.flags.runtime == "linux-docker") {
-      backend = "docker";
-      containers = {
-        bitbucket-runner = {
-          image = "docker-public.packages.atlassian.com/sox/atlassian/bitbucket-pipelines-runner";
-          volumes = [
-            "/tmp:${cfg.flags.workingDirectory}"
-          ];
-          environment =
-            {
-              ACCOUNT_UUID = cfg.flags.accountUuid;
-              RUNNER_UUID = cfg.flags.runnerUuid;
-              OAUTH_CLIENT_ID = cfg.flags.OAuthClientId;
-              OAUTH_CLIENT_SECRET = cfg.flags.OAuthClientSecret;
-              WORKING_DIRECTORY = cfg.flags.workingDirectory;
-            }
-            ++ lib.optionalAttrs (cfg.flags.repositoryUuid != null) {
-              REPOSITORY_UUID = cfg.flags.repositoryUuid;
-            };
+    systemd.services = lib.mapAttrs' (
+      name: runner:
+      let
+        execArgs = [
+          "--accountUuid {${runner.accountUuid}}"
+          "--runnerUuid {${runner.runnerUuid}}"
+          "--OAuthClientId ${runner.OAuthClientId}"
+          "--OAuthClientSecret ${runner.OAuthClientSecret}"
+          "--runtime ${runner.runtime}"
+          "--workingDirectory ${runner.workingDirectory}"
+        ] ++ lib.optional (runner.repositoryUuid != null) "--repositoryUuid {${runner.repositoryUuid}}";
+      in
+      nameValuePair "bitbucket-runner-${name}" {
+        description = "Bitbucket Runner ${name}";
+        after = [ "network.target" ];
+        wantedBy = [ "multi-user.target" ];
+        path = cfg.extraPackages;
+        serviceConfig = {
+          ExecStart = "${bitbucketRunner}/bin/bitbucket-runner-linux-shell ${lib.concatStringsSep " " execArgs}";
+          User = cfg.user;
+          Group = cfg.group;
+          Restart = "on-failure";
         };
-      };
-    };
+      }
+    ) (filterAttrs (_: runner: runner.runtime == "linux-shell") cfg.runners);
+
+    virtualisation.oci-containers.containers = lib.mapAttrs' (
+      name: runner:
+      nameValuePair "bitbucket-runner-${name}" {
+        image = "docker-public.packages.atlassian.com/sox/atlassian/bitbucket-pipelines-runner";
+        volumes = [ "/tmp:${runner.workingDirectory}" ];
+        environment =
+          {
+            ACCOUNT_UUID = runner.accountUuid;
+            RUNNER_UUID = runner.runnerUuid;
+            OAUTH_CLIENT_ID = runner.OAuthClientId;
+            OAUTH_CLIENT_SECRET = runner.OAuthClientSecret;
+            WORKING_DIRECTORY = runner.workingDirectory;
+          }
+          // lib.optionalAttrs (runner.repositoryUuid != null) {
+            REPOSITORY_UUID = runner.repositoryUuid;
+          };
+      }
+    ) (filterAttrs (_: runner: runner.runtime == "linux-docker") cfg.runners);
   };
+
 }
