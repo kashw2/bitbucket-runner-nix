ddc0f0e7b7575436a0862a12604f64277a41e5b2
kashw2 <supra4keanu@hotmail.com>
2026-06-28 00:19:57 +1000

    fix(flake): incorrect pkgs invocation

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system}.pkgs;
+      pkgs = nixpkgs.legacyPackages.${system};
     in
     {
       packages.${system} = {
